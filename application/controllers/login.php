9a41a0ec553d383d887e3c01d5ce948be42e2443
Jojo Arianto <jochkonly@gmail.com>
2015-03-31 21:06:19 +0700

    Start Coding
    Framework Codeigniter

---
+++ application/controllers/login.php
@@ -0,0 +1,16 @@
+<?php 
+	/**
+	* 
+	*/
+	class Login extends CI_Controller
+	{
+		
+		public function index(){
+			echo "Hello World";
+		}
+
+		public function comment(){
+			echo "Sukses besar";
+		}
+	}
+ ?>
