0b58051324194150dcd54855a9c95a98a5225469
Rizqi Sulistianto <rizqisima@gmail.com>
2015-04-08 22:46:28 +0700

    Interface Sistem Antrian On The Spot
    Interface Sistem Antrian On The Spot

---
+++ PROJECT/interface/Sistem Antrian On The Spot(Rizqi)/antri.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="container">
+        <form>
+
+          <h1 class="text-center">Muchi Clinic</h1>
+          <h1 class="text-center"> Sistem Antri On The Spot</h1>
+        
+            <div class="row">
+              <div class="col-md-6 text-center">
+                <br>
+                <table border="0" cellspacing="0" cellpadding="0"><tr><td align="center"><script type="text/javascript" 
+                src="http://www.worldtimeserver.com/clocks/embed.js"></script><script type="text/javascript" language="JavaScript">objUSNY=new Object;objUSNY.wtsclock="wtsclock001.swf";objUSNY.color="87CEEB";objUSNY.wtsid="US-NY";
+                objUSNY.width=200;objUSNY.height=200;objUSNY.wmode="transparent";showClock(objUSNY);</script></td></tr><tr><td 
+                align="center"><h2>Surabaya</h2></td></tr></table>
+              </div>
+
+              <div class="col-md-6 text-center">
+                <br>
+                <h1> Nomor Antrian Saat Ini</h1>
+                <h1 style"font-size:="" 150px;"="" style="font-size: 150px;">001</h1>
+              </div>
+            </div>
+
+          <form class="form-horizontal">
+            <div class="form-group">
+              <label for="inputEmail3" class="col-sm-2 control-label">Tanggal :</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Senin, 12 April 2015" disabled>
+              </div>
+
+              <label for="inputEmail3" class="col-sm-2 control-label">Nama dokter :</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Dokter Irfan" disabled>
+              </div>
+
+              <label for="inputEmail3" class="col-sm-2 control-label">ID pasien :</label>
+              <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="ABCDEF123" disabled>
+              </div>
+            <br>
+            <br>
+              
+            <button type="button" class="btn btn-default btn-lg">Input ID pasien</button>
+          </form>
+      </div>
+    </div>
+
+    
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
