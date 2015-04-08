5d20501010082e4b381ecdc0f393e39927576072
Fitria Nur Izzatin Faza <fitria.nur-12@fst.unair.ac.id>
2015-04-09 00:23:40 +0700

    interface absensi
    interface absendi

---
+++ PROJECT/interface/Absensi (Fitria NUr Izzatin Faza)/absensi.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <th></th>
+  </head>
+  <body>
+    <form>
+      <table border="0" align="center">  
+      <tr>
+          <td align="center"><h1><b>Hallo, Selamat Datang, </b></h1></td>
+      </tr>
+    </table>
+    <table align="center">
+        <tr>
+          <td><img src="s.png" width="200px" height="200px"></td>
+        </tr>
+    </table>
+      <table border="0" align="center">  
+        <tr>
+          <td>Nomor ID</td><br><br>
+          <td><input type="text" name="text"></td>
+        </tr>
+      </table>
+      <table align="center">
+          <tr>
+          <td align="center"><input type="submit" name="txtabsen" value="absen"></td>
+        </tr>
+      </table>
+      <table align="center">
+          <tr>
+          <td align="center"><h3><b><marquee>Jam Saat Ini 07.17</marquee></b></h3></td>
+        </tr>
+      </table>
+     </form> 
+  </body>
+</html>
