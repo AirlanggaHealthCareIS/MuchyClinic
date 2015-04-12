3cc095e3167194eb1d51d472a1a589f671d3c934
donyprasetiyo <dprasetiyo63@gmail.com>
2015-04-11 18:50:44 -0700

    header dan footer
    header dan footer telah di upload

---
+++ PROJECT/application/views/v_header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Muchy Clinic2 | Buat Jadwal</title>
+
+    <!-- Bootstrap -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    
+  </head>
+  <body>
+    <div class="container-fluid text-center" style="background-color: #606062">
+      <img src="<?php echo base_url(); ?>assets/image/header1.png" class="" alt="Responsive image">
+    </div>
