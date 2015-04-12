418f8856c167cd1060a171a29f4af38cd4816cbc
Fitria Nur Izzatin Faza <fitria.nur-12@fst.unair.ac.id>
2015-04-12 13:06:42 +0700

    fitur absensi
    fitur absensi skenario D - text field ID karyawan tidak di isi oleh
    aktor, dan menampilkan pesan peringatan.
    Obstacle : masih pemula, sehingga membutuhkan waktu yang lama
    To do : Mengerjakan skenario A apabila user salah input.

---
+++ PROJECT/application/controllers/cabsensi.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Cabsensi extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('v_header');
+		$this->load->view('absensi/v_absensi');
+	}
+	
+	public function panggil()
+	{
+		$this->load->view('v_header');
+		$this->load->view('absensi/v_absensi');
+	}
+	public function validasi()
+	{
+		$id = $this->input->post('id_karyawan');
+		echo $id;
+		if($id==null|| $id==""){
+			//echo "kok kosong";
+			redirect(base_url().'cabsensi?error=null');
+		}
+		else{
+			echo $id;
+		}
+	}
+}
