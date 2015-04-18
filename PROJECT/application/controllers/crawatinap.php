afd783c93d08dcc3ec563c613bd93568f3273efd
irfan nur aulia <irfan.nur-12@fst.unair.ac.id>
2015-04-19 06:44:55 +0700

    fitur rawat inap
    DO : skenario A masih 50% , validasi input seperti simbol-simbol yang
    tidak diizinkan sedang proses, mencoba menampilkan data-data yang
    melakukan rawat inap juga sedang proses, sedikit sedikit mengetahui web
    dengan code igniter
    OBSTACLE : manajemen waktu ..
    TO DO : mencoba menyelesaikan skenario selanjutnya . masih mencari
    informasi cara menampilkan tanggal dengan CI serta menghitung selisih ..

--- PROJECT/application/controllers/crawatinap.php
+++ PROJECT/application/controllers/crawatinap.php
@@ -3,19 +3,129 @@
 class Crawatinap extends CI_Controller {
 	public function index()
 	{
+		$data = array("id_pasien"=> " ", "nama_pasien"=> " ", "no_telp_pas"=> " ");
+
 		$this->load->view('v_header');
-		$this->load->view('rawatinap/rawatinap');
-		$this->load->view('v_footer');
+		$this->load->view('rawatinap/rawatinap', $data);
+		$this->load->view('v_n_footer');	
+		
+	}
+
+	public function forminap()
+	{
+		$data = array("id_pasien"=> " ", "nama_pasien"=> " ", "no_telp_pas"=> " ");
+
+		$this->load->view('v_header');
+		$this->load->view('rawatinap/rawatinap', $data);
+		$this->load->view('v_n_footer');
+	}
+	public function daftarinap()
+	{
+
+		// $this->load->database();
+		// $this->load->model('mrawatinap');
+		// $query = $this->mrawatinap->getinap();
+		// $ro = $query->row();
+		$this->load->database();
+		$data[] = $this->mrawatinap->getinap();
+
+		$this->load->view('v_header');
+		$this->load->view('rawatinap/daftarinap');
+		$this->load->view('v_n_footer');
 	}
 	public function validasi()
 	{
-		$id_pas = $this->input->post('id_pasien');
-		if ($id_pas == null || $id_pas == ""){
+		$this->load->helper(array('form', 'url'));
+
+        $this->load->library('form_validation');
+		$id= $this->input->post('id_pasien');
+		if ($id == null || $id == ""){
 			// echo "kossoonngg!!!, isien disik";
 			redirect(base_url().'crawatinap?error=null 	');
 		}
 		else {
-			echo $id_pas;
+			$this->tampilid($id);
+			
 		}
-	}	
+	}
+
+	public function tampilid($id){
+		$this->load->database();
+		
+		$id = $this->input->post('id_pasien');
+		$this->load->model('mrawatinap');
+		$query = $this->mrawatinap->getcariid($id);
+		$ro = $query->row();
+		$data = array("id_pasien"=> $ro->ID_PASIEN, "nama_pasien"=> $ro->NAMA_PASIEN, "no_telp_pas"=> $ro->NO_TLP_PAS_);
+
+		$this->load->view('v_header');
+		$this->load->view('rawatinap/rawatinap', $data);
+		$this->load->view('v_n_footer');
+	}
+
+	// public function contomodel(){
+	// 	$this->load->database();
+	// 	$this->load->model('mrawatinap');
+	// 	$call = $this->mrawatinap->getcariid(1);
+	// 	$ro = $call->row();
+	// 	echo $ro->NAMA_PASIEN;
+	// }
+	// public function rawat()
+	// {
+	// 	$this->load->view('v_header');
+	// 	$this->load->view('rawatinap/rawatinap');
+	// 	$this->load->view('v_n_footer');
+		
+	// }
+	// public function menurawatinap()
+	// {
+	// 	$this->load->view('v_header');
+	// 	$this->load->view('rawatinap/menurawatinap');
+	// 	$this->load->view('v_n_footer');
+	// }
+
+	// hanya mencoba
+	// public function doRegRawatInap()
+	// {
+	// 	$required = array('nim', 'nama', 'alamat_as', 'kota', 'provinsi', 'telp', 'fak', 'email', 'jalur', 'nm_ayah', 'al_ayah', 'telp_ayah', 'pkrjn_ayah', 'nm_ibu', 'al_ibu', 'telp_ibu', 'pkrjn_ibu', 'hsl_ibu', 'jml_anak');
+ //        $error = false;
+ //        foreach ($required as $field) {
+ //            if (empty($_POST[$field])) {
+ //                $error = true;
+ //            }
+ //        }
+ //        if (!$error) {
+ //            $nim = $_POST['nim'];
+ //            $nama = $_POST['nama'];
+ //            $alamat = $_POST['alamat_as'];
+ //            $kota = $_POST['kota'];
+ //            $provinsi = $_POST['provinsi'];
+ //            $telp = $_POST['telp'];
+ //            $fakultas = $_POST['fak'];
+ //            $mail = $_POST['email'];
+ //            $jalur = $_POST['jalur'];
+ //            $nm_ayah = $_POST['nm_ayah'];
+ //            $almt_ayah = $_POST['al_ayah'];
+ //            $telp_ayah = $_POST['telp_ayah'];
+ //            $pkrjn_ayah = $_POST['pkrjn_ayah'];
+ //            $nm_ibu = $_POST['nm_ibu'];
+ //            $almt_ibu = $_POST['al_ibu'];
+ //            $telp_ibu = $_POST['telp_ibu'];
+ //            $pkrjn_ibu = $_POST['pkrjn_ibu'];
+ //            $gaji_ortu = $_POST['hsl_ibu'];
+ //            $anak = $_POST['jml_anak'];
+ //            $foto = $_FILES['photo']['tmp_name'];
+ //            $foto1 = $_FILES['photo1']['tmp_name'];
+ //            $foto2 = $_FILES['photo2']['tmp_name'];
+ //            $foto3 = $_FILES['photo3']['tmp_name'];
+ //            $tgldftr = date('Y-m-d');
+ //            parent::loadModel('Registrasi');
+ //            $this->model->insertRegistrasi($nim, $nama, $alamat, $kota, $provinsi, $telp, $fakultas, $mail, $jalur, $nm_ayah, $almt_ayah, $telp_ayah, $pkrjn_ayah, $nm_ibu, $almt_ibu, $telp_ibu, $pkrjn_ibu, $gaji_ortu, $anak, $foto, $foto1, $foto2, $foto3, $tgldftr);
+ //            echo "<script type='text/javascript'>alert('Input Succesfully!')</script>";
+ //            $this->view->renderAdm('administrator/registrasi');
+ //        } else {
+ //            echo "<script type='text/javascript'>alert('Check your input!')</script>";
+ //            $this->view->renderAdm('administrator/registrasi');
+ //        }
+	// }
 }
--- PROJECT/application/views/rawatinap/rawatinap.php
+++ PROJECT/application/views/rawatinap/rawatinap.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Rawat inap</title>
-    <style type="text/css">
-
-    </style>
-
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="<?php echo base_url(); ?>assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
   <body>
     <div class="container-fluid" style="margin-bottom:50px;">
       <div class="container">
@@ -33,40 +8,40 @@
             <!-- pesan error -->
             <?php if ($this->input->get('error')=='null'): ?>
               <div class="alert alert-danger" role="alert">
-                maaf
+                <p>Please Input ID Pasien Correctly!</p>
               </div>
             <?php endif ?>
             <!-- pesan error -->
             <form class="form-inline" action="<?php echo base_url()?>crawatinap/validasi" method="post">
               <div class="form-group">
-                <label class="sr-only" for="exampleInputEmail3">ID Pasien</label>
-                <input type="normal" name="id_pasien" class="form-control" id="exampleInputEmail3" placeholder="ID Pasien">
+                <label class="sr-only" for="exampleInputEmail3" >ID Pasien</label>
+                <input type="normal" name="id_pasien" maxlength="5" class="form-control" id="exampleInputEmail3" placeholder="ID Pasien">
               </div>
               
               <button type="submit" class="btn btn-default">search</button>
               <br></br>
 
-              <table class="" style="border: 0px currentColor; border-image: none;">
+              <table class="table table-bordered" style="border: 0px currentColor; border-image: none;">
+                
                 <tr>
-                <td style="width:43%">ID pasien</td>
-                <td>PAS01</td>
+                <td class="info">Id Pasien</td>
+                <td class="info"> <?php echo $id_pasien ?></td>
                 </tr>
                 <tr>
-                <td>Nama pasien</td>
-                <td>Doni Prasetyo</td>
-                </tr>
-                <tr>
-                <td>TTL</td>
-                <td>Samarinda, 30 Februari 1880</td>
+                <td class="info">Nama Pasien</td>
+                <td class="info"><?php echo $nama_pasien ?></td>
                 </tr>
 
                 <tr>
-                <td>Jenis Kelamin</td>
-                <td>Laki-Laki</td>
+                <td class="info">no telp</td>
+                <td class="info"><?php echo $no_telp_pas ?></td>
                 </tr>
+
+        
             </table><br>
-
-            </form>
+            <!-- TAABBBEEELLL -->
+        <!-- TAABBBEEELLL --> 
+        </form>
 
           </div>
           <div class="col-md-4">
@@ -94,37 +69,20 @@
             <div class="row">
               <div class="col-xs-6">
                 <label for="disabledTextInput">tanggal masuk</label>
-            <div class="form-group">
-                <label class="sr-only" for="exampleInputEmail3">Tanggal masuk</label>
-                <input type="text" class="form-control" id="exampleInputEmail3" placeholder="tanggal masuk">
-            </div>
+                  <div class="form-group">
+                      <label class="sr-only" for="exampleInputEmail3">Tanggal masuk</label>
+                      <input type="text" class="form-control" id="exampleInputEmail3" placeholder="tanggal masuk">
+                  </div>
               </div>
               <div class="col-xs-6">
                 <label for="disabledTextInput">tanggal keluar</label>
-            <div class="form-group">
-                <label class="sr-only" for="exampleInputEmail3">Tanggal keluar</label>
-                <input type="text" class="form-control" id="exampleInputEmail3" placeholder="tanggal keluar">
-            </div>
-              </div>
-            </div>
-
-            <label for="disabledTextInput">Tunai</label>
-              <div class="row">
-                <div class="col-md-8">
-                  
-                  <label class="sr-only" for="exampleInputAmount">Tunai (in rupiah)</label>
-                  <div class="input-group">
-                    <div class="input-group-addon">Rp.</div>
-                    <input type="text" class="form-control" id="exampleInputAmount" placeholder="4.132.000">
-                    <div class="input-group-addon">.00</div>
+                  <div class="form-group">
+                      <label class="sr-only" for="exampleInputEmail3">Tanggal keluar</label>
+                      <input type="text" class="form-control" id="exampleInputEmail3" placeholder="tanggal keluar">
                   </div>
-                </div>
-                <div class="col-md-4">
-                  
-                  <br></br>
-                </div>
               </div>
-          </div>
+            </div><br>
+            </div>
         
           <div class="col-md-4">
 
@@ -132,8 +90,8 @@
           <div class="col-md-4">
           </div>
         </div>
-        <button type="submit" class="btn btn-default">submit</button>
-        <button type="reset" class="btn btn-default">cancel</button>
+        <button type="submit" class="btn btn-primary">submit</button>
+        <button type="reset" class="btn btn-primary">cancel</button>
       </div>
     </div>
 
@@ -142,4 +100,3 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
   </body>
-</html>
