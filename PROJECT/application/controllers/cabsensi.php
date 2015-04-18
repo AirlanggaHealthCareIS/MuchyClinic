61450cd9913cd96fb1c2eb97422c5ec477833cbf
Fitria Nur Izzatin Faza <fitria.nur-12@fst.unair.ac.id>
2015-04-19 05:16:38 +0700

    Fitur Absensi
    Do : Skenario A (kurang pesan untuk feedback)
    Obstacle : database belum selesai
    To Do : penyempurnaan skenario

--- PROJECT/application/controllers/cabsensi.php
+++ PROJECT/application/controllers/cabsensi.php
@@ -5,23 +5,46 @@ class Cabsensi extends CI_Controller {
 	{
 		$this->load->view('v_header');
 		$this->load->view('absensi/v_absensi');
+		$this->load->view('v_footer');	
+		
+
+
+			
+
+
+	}
+	
+	private function input()
+	{
+		$idk = $this->input->post('id_karyawan');
+		$timek=$this->input->post('time_masuk');
+		$datek=$this->input->post('date_masuk');
+		$this->load->model("m_absensi");
+		$inputA = $this->m_absensi->getAbsensi($idk,$timek,$datek);
 	}
 	
 	public function panggil()
 	{
-		/*$this->load->view('v_header');
-		$this->load->view('absensi/v_absensi');*/
+		
 	}
 	public function validasi()
 	{
 		$id = $this->input->post('id_karyawan');
+		$time=$this->input->post('time_masuk');
+		$date=$this->input->post('date_masuk');
 		echo $id;
 		if($id==null|| $id==""){
 			//echo "kok kosong";
 			redirect(base_url().'cabsensi?error=null');
 		}
+		if($time==null| $time=""){
+			redirect(base_url().'cabsensi?error=null');
+		}
+		if($date==null| $date=""){
+			redirect(base_url().'cabsensi?error=null');
+		}
 		else{
-			echo $id;
+			$this->input($id);
 		}
 	}
 }
