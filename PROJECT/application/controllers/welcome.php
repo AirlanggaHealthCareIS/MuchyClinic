926cf72ed367c80179fe56ae315093469e92c483
ismul zamroni <ismulzamroni@gmail.com>
2015-04-19 22:05:13 +0700

    menu login
    hambatan belum ada
    do mengerjakan controller yang di beri tanda error pada setiap perintah
    yang belum benar dan lengkap
    to do mengerjakan menu login secara sempurna

--- PROJECT/application/controllers/welcome.php
+++ PROJECT/application/controllers/welcome.php
@@ -2,8 +2,56 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('login/v_login');
+		// $username = $this->getcoba_psi(1);
+		// echo $username;
+
+
+		// $this->load->database();
+		// $this->load->model('m_masuk');
+		// $call = $this->m_masuk->getcoba_psi(1);
+		// $ro = $call->row();
+		// echo $ro->username;
 	}
+	public function panggil()
+	{
+		$this->load->view('header');
+		$this->load->view('login/v_login');
+		$this->load->view('m_masuk');
+	}
+	
+	public function validasi()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		if (($username==null || $username=="") && ($password==null || $password=="")) {
+			redirect(base_url().'Welcome?error=null');
+		} else {
+			echo "$username && $password";
+		}
+		
+		if ($username==null || $username=="") {
+			redirect(base_url().'Welcome?error=nullusername');
+		} else {
+			echo "$username";
+		}
+
+		if ($password==null || $password=="") {
+			redirect(base_url().'Welcome?error=nullpassword');
+		} else {
+			echo "$password";
+		}
+		
+	}
+	private function getcoba_psi($ido){
+		$query = $this->db->query('SELECT * FROM `karyawan` WHERE `id` = '.$ido);
+		$row = $query->row();
+		return $row->username;
+	}
+	
+
 }
