cf974c9a63afc45fde2d262c965efb2dca9c5dad
Jojo Arianto <jochkonly@gmail.com>
2015-04-27 18:15:33 +0700

    Dashboard
    DO : Membuat tamplate untuk dashboard
    OBSTACTLE : Sementara ini masih belum ada masalah
    TO DO : Mengintegrasikan semua fitur

---
+++ PROJECT/application/views/old/v_footer.php
@@ -0,0 +1,14 @@
+<div class="container-fluid text-center" style="background-color: rgb(105, 102, 102);">
+        <h6 style="color: white;">copyright | Muchy Team</h6>
+      </div>
+
+    </div>
+    
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- PROJECT/application/views/v_footer.php
+++ PROJECT/application/views/v_footer.php
@@ -1,14 +1,26 @@
-<div class="container-fluid text-center" style="background-color: rgb(105, 102, 102);">
-        <h6 style="color: white;">copyright | Muchy Team</h6>
-      </div>
-
+</div>	
     </div>
-    
+</div>
+	<!-- <div class="container-fluid text-center" style="background-color: rgb(105, 102, 102); padding:15px">
+		<h6 style="color: white;">copyright | Muchy Team</h6>
+	</div> -->
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- // <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <!-- // <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script> -->
+    <!-- jQuery -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+    <!-- Morris Charts JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/bower_components/raphael/raphael-min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/bower_components/morrisjs/morris.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/morris-data.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/dist/js/sb-admin-2.js"></script>
   </body>
 </html>
