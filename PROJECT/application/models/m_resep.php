5d653370618628252eef98452e3e94c8f6247427
dianramadhan <dianramadhan1194@yahoo.com>
2015-05-04 12:28:10 +0700

    menambah query di m_resep
    do: menambah query
    
    hobstacle: -
    
    to do: upload revisi

--- PROJECT/application/models/m_resep.php
+++ PROJECT/application/models/m_resep.php
@@ -10,17 +10,23 @@ class M_resep extends CI_Model {
         }
         
         public function getcariid ($idpemeriksaan){
-                $query = $this->db->query("SELECT P.`ID_PERIKSA` , PA.`ID_PASIEN` , PA.`NAMA_PASIEN` , D.`ID_DOKTER` , D.`NAMA_DOKTER` FROM `PEMERIKSAAN` AS P , `PASIEN` AS PA , `DOKTER` AS D WHERE P.`ID_PASIEN` = PA.`ID_PASIEN` AND P.`ID_DOKTER` = D.`ID_DOKTER`");
+        
+                $query = $this->db->query("SELECT P.`ID_PERIKSA` , PA.`ID_PASIEN` , PA.`NAMA_PASIEN` , D.`ID_DOKTER` , D.`NAMA_DOKTER` FROM `PEMERIKSAAN` AS P , `PASIEN` AS PA , `DOKTER` AS D WHERE P.`ID_PASIEN` = PA.`ID_PASIEN` AND P.`ID_DOKTER` = D.`ID_DOKTER` AND P.`ID_PERIKSA` = '".$idpemeriksaan."'");
                 return $query;
         }
         
         public function insert ($idresep,$idpemeriksaan,$idpasien,$iddokter,$tglresep){
-                $query = $this->db->query("INSERT INTO resep (`ID_RESEP`, `ID_PASIEN`, `ID_DOKTER`, `ID_PERIKSA` , `TGL_RESEP`) VALUES ('".$idresep."','".$idpasien."','".$idpemeriksaan."','".$iddokter."','".$tglresep."')");
+                $query = $this->db->query("INSERT INTO resep (`ID_RESEP`, `ID_PASIEN`, `ID_DOKTER`, `ID_PERIKSA` , `TGL_RESEP`) VALUES ('".$idresep."','".$idpasien."','".$iddokter."','".$idpemeriksaan."','".$tglresep."')");
                 return $query;
-        }  
+                }  
 
         public function getResep($id, $tgl){
                 $query = $this->db->query("SELECT * FROM resep AS r, dokter AS d, pasien AS p WHERE r.ID_DOKTER = d.ID_DOKTER AND r.ID_PASIEN=p.ID_PASIEN AND r.ID_PASIEN='".$id."' AND TGL_RESEP='".$tgl."'");
+                return $query;
+        }
+
+        public function getobat ($idobat){
+                $query = $this->db->query("SELECT O.`ID_OBAT` , O.`NAMA_OBAT` FROM `OBAT` AS O WHERE O.`ID_OBAT` = '".$idobat."'");
                 return $query;
         }
 
@@ -28,6 +34,15 @@ class M_resep extends CI_Model {
                 $query = $this->db->query("SELECT * FROM detail_resep AS d, obat AS o WHERE d.ID_OBAT = o.ID_OBAT AND d.ID_RESEP='".$idr."'");
                 return $query;
         } 
+         public function insertresep ($iddetailresep,$idobat,$idresep,$ketobat){
+                $query = $this->db->query("INSERT INTO detail_resep (`ID_DETAIL_RESEP`, `ID_OBAT`, `ID_RESEP`, `KET_RESEP`) VALUES ('".$iddetailresep."','".$idobat."'.'".$idresep."'.'".$ketobat."')");
+                return $query;
+        }
+
+        public function getidresep($idresep){
+                $query = $this->db->query("SELECT R.`ID_RESEP` FROM `RESEP` AS R WHERE R.`ID_RESEP` = '".$idresep."'");
+                return $query;
+        }
                 
 
 }
