67bc694597042d01a0a8f909b7e7cbb9e513c69b
donyprasetiyo <dprasetiyo63@gmail.com>
2015-05-11 21:18:57 +0700

    BDD + TDD dan Upload Sequence yang minggu lalu
    Do : mengerjakan BDD + TDD
    Obstacle :
    To Do : Mengerjakan php unit yang belum selesai dan melengkapi scenario
    yang belum terselesaikan

--- PROJECT/application/controllers/pendaftaran.php
+++ PROJECT/application/controllers/pendaftaran.php
@@ -1,7 +1,7 @@
 <?php
 // defined('BASEPATH') OR exit('No direct script access allowed');
 
-class pendaftaran  { // extends CI_Controller
+class pendaftaran extends CI_Controller  { // extends CI_Controller
 
 
 
