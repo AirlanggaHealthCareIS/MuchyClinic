a9d0adf8b7e2da3f96dabe507f35583b7f3b55a8
ismul zamroni <ismulzamroni@gmail.com>
2015-05-18 19:27:54 +0700

    fitur model login apoteker
    do mengerjakan model untuk memanngil login apoteker dr database
    obstacle belum ada
    to do mengerjakan php unit tp masih belum bisa

--- PROJECT/application/models/m_login.php
+++ PROJECT/application/models/m_login.php
@@ -32,6 +32,20 @@ class m_login extends CI_Model {
 
         
         }
+
+         public function getapoteker($username,$password)
+        {
+                // $query = $this->db->query("SELECT * FROM `dokter` WHERE NAMA_DOKTER = '".$username."' OR PASSWORD = '".$password."'");
+                
+                $query = $this->db->query("SELECT * FROM `apoteker` WHERE NAMA_APOTEKER = '".$username."' OR PASSWORD = '".$password."'");
+                if ($query->num_rows() ==1) {
+                        return true;
+                } else {
+                        return false;
+                }
+
+        
+        }
 }
 
  ?>
