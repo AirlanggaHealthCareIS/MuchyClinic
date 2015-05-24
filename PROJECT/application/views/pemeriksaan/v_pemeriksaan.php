5faf6c7c1e2459d68155dd9a74a6b02e07fe505b
Fitria Nur Izzatin Faza <fitria.nur-12@fst.unair.ac.id>
2015-05-24 22:35:35 +0700

    Pemeriksaan
    do : mengerjakan interface pemeriksaan
    obstacle : banyak tugas
    todo : menyelesaikan semua fitur

---
+++ PROJECT/application/views/pemeriksaan/v_pemeriksaan.php
@@ -0,0 +1,42 @@
+    <div class="container-fluid" style = "padding: 100px">
+      <div class="row"> 
+        <div class="col-md-6 col-md-offset-3">
+          <h1 class = "text-center">Pemeriksaan</h1>
+            <div class = "container-fluid text-center">
+              <img src = "<?php echo base_url()?>assets/images/logo2.png" alt="Responsive image"> 
+            </div>
+            <form>
+  			<div class="form-group">
+    		<label for="exampleInputEmail1">ID Periksa : </label>
+    		<input type="normal" class="form-control" id="idperiksa" placeholder="Id Periksa">
+  			</div>
+            <div class="form-group">
+    		<label for="exampleInputEmail1">ID Pasien : </label>
+    		<input type="normal" class="form-control" id="idpasien" placeholder="Id Pasien">
+  			</div>  
+  			<div class="form-group">
+    		<label for="exampleInputEmail1">ID Dokter : </label>
+    		<input type="normal" class="form-control" id="iddokter" placeholder="Id Dokter">
+  			</div>
+  			<div class="form-group">
+    		<label for="exampleInputEmail1">ID Penyakit : </label>
+    		<input type="normal" class="form-control" id="idpenyakit" placeholder="Id Penyakit">
+  			</div>
+  			<div class="form-group">
+    		<label for="exampleInputEmail1">ID Periksa : </label>
+    		<input type="normal" class="form-control" id="idperiksa" placeholder="Id Periksa">
+  			</div>
+  			<div class="form-group">
+    		<label for="exampleInputEmail1">Tanggal Periksa: </label>
+    		<input type="normal" class="form-control" id="tanggalperiksa" placeholder="tanggal Periksa">
+  			</div>
+  			<div class="form-group">
+    		<label for="exampleInputEmail1">Biaya Periksa </label>
+    		<input type="normal" class="form-control" id="biayaperiksa" placeholder="biaya periksa">
+  			</div>
+  			<button type="submit" class="btn btn-default">deal</button>
+            </form>
+
+        </div>
+      </div>
+    </div>
