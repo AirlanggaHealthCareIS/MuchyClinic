89fb25b2345425a85959fe8fce296dfeed13e869
Rizqi Sulistianto <rizqisima@gmail.com>
2015-05-27 07:09:59 +0700

    Update model fitur Sistem Antri On The Spot
    DO : Update model fitur Sistem Antri On The Spot
    
    OBSTACLE : -
    
    TO DO : Mengerjakan scenario yang lainnya, dan mengerjakan database

--- PROJECT/application/models/m_antrian.php
+++ PROJECT/application/models/m_antrian.php
@@ -10,7 +10,7 @@ class M_antrian extends CI_Model {
         }
 
    		public function getAntri($idp){
-			$query = $this->db->query('SELECT * FROM `pasien` WHERE `id_pasien`='.$idp);
+			$query = $this->db->query("SELECT * FROM `pasien` WHERE `ID_PASIEN` = '".$idp."'");
 			/*$row = $query->row();*/
 			return $query;
 		}
@@ -19,10 +19,33 @@ class M_antrian extends CI_Model {
             /*$row = $query->row();*/
             return $query->result();
         }
-        public function insert($id_dokter, $id_pasien){
-            $query = $this->db->query("INSERT INTO WAITING_LIST(`ID_PASIEN`, `ID_DOKTER`) VALUES ('".$id_pasien."', '".$id_dokter."')");
+        public function insert($no_antri, $id_pasien, $id_dokter, $tgldftr, $setAntri){
+            $query = $this->db->query("INSERT INTO WAITING_LIST(`ID_ANTRI`, `ID_PASIEN`, `ID_DOKTER`, `NO_ANTRI`, `TGL_ANTRI`) VALUES ('".$no_antri."', '".$id_pasien."', '".$id_dokter."','".$setAntri."', '".$tgldftr."')");
             return $query;
         }
+        public function antrian($tanggal){
+            $query = $this->db->query("SELECT count(*) AS tanggal FROM `waiting_list` where TGL_ANTRI = '".$tanggal."'");
+            $query = $query->row();
+
+            return $query->tanggal;
+        }
+        public function countAntrian()
+        {
+            $query = $this->db->query("SELECT count(*) AS W FROM `waiting_list`");
+            if ($query->num_rows()>0) {
+              return $query->row()->W;
+            } else {
+              return 0;
+            }
+          }
+
+          public function generateAntrian(){
+            $id = $this->countAntrian() + 1;
+            if ($id < 10) $id = "AT00".$id;
+            else if ($id < 100) $id = "AT0".$id;
+            else if ($id < 1000) $id = "AT".$id; 
+            return $id;
+          }
 }
 	
 
